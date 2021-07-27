1f30598479d7ed57aa3d2d587feb14b4c4e21427
Fullonikon <maxim-199910@mail.ru>
2021-07-27 14:18:05 +0300

    add comments

--- ReadNal/ReadNal.cpp
+++ ReadNal/ReadNal.cpp
@@ -6,55 +6,51 @@
 
 using namespace std;
 
-string readNal(string input, int startPosition, int &size)
-{
+string readNal(string input, int startPosition, int &size) // считывание блока нал из входного потока с введённого символа
+{                                                          // размер для вывода
     string nal;
-    string nalSize1;
+    string nalSize;
     for (int i = startPosition; i < startPosition + 8; i++)
     {
-        nalSize1 += input[i];
+        nalSize += input[i];
     }
-    size = stoi(nalSize1, 0, 16)*2;
-    nal += nalSize1;
+    size = stoi(nalSize, 0, 16)*2; // умножить на два потому что бит это символа 
+    nal += nalSize;
     nal = input.substr(startPosition, size+8);
+
 
     return nal;
 }
 
-int main()
+void main()
 {
     unsigned char x;
-    //std::ifstream input("C:/Users/ami/Desktop/cats/segment8.fmp4", std::ios::binary);
-    std::ifstream input("C:/Users/ami/Desktop/Test_zapis_34_original_iso_fragmented/fileSequence0.mp4", std::ios::binary);
+    std::ifstream input("C:/Users/ami/Desktop/cats/segment2.fmp4", std::ios::binary); // ввод
     stringstream stream;
     input >> std::noskipws;
-    while (input >> x) {
+    while (input >> x) { // чтение данных
         stream << std::hex << std::setw(2) << std::setfill('0')
             << (int)x;
-        cout << x;
     }
     string f;
     string nalTmp;
-    f = stream.str().substr(stream.str().find("6d646174")+8);
-    string nalSizeStr;
-    for (int i = 0; i < 8; i++)
-    {
-        nalSizeStr += f[i];
-    }
-    int nalSize; // = stoi(nalSizeStr, 0, 16);
-    nalTmp = readNal(f, 0, nalSize);
-    cout << nalTmp << "  ";
-    //nalSize = stoi(nalTmp.substr(0, 8), 0, 16);
-    nalTmp = readNal(f, nalSize+8, nalSize);
-    cout << nalTmp;
+    f = stream.str().substr(stream.str().find("6d646174")+8); // поиск начала
     
 
     int curPos = 0;
-    for (int i = 0; i < 10; i++)
+    while (true)
     {
         int size;
         nalTmp = readNal(f, curPos, size);
-        char nalType = nalTmp[9];
+        char nalType = nalTmp[9]; // здесь лежит тип
+        if (nalType == '5') {
+            cout << "WE WON";
+            return;
+        } else
+            if (nalType == '1') {
+                cout << "Wrong nal unit start";
+                return;
+            }
         curPos += size + 8;
     }
 }
